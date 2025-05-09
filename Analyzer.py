e1fe3e6a8d6acf57d6c2d25c945c0c500599ed16
Drew Sommer <drew@sommerengineering.com>
2025-05-09 05:30:47 -0700

    feat: Stub live framework

--- Analyzer.py
+++ Analyzer.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import Repository as repo
 from Engine import Engine
-from SMACrossoverStrategy import SMACrossoverStrategy
+from HalfwayStrategy import HalfwayStrategy
 import logging
 
 plt.rcParams['figure.figsize'] = [20, 12]
@@ -10,15 +10,13 @@ logging.getLogger('matplotlib.font_manager').setLevel(logging.ERROR)
 # get ohlc prices
 csv_filename = "data/nq_3months_2025-02-01_2025-05-01.csv"
 data = repo.getOhlc(csv_filename=csv_filename) # repo.getOhlc()
-data['sma_12'] = data.Close.rolling(12).mean()
-data['sma_24'] = data.Close.rolling(24).mean()
 
 # init engine
 engine = Engine(initial_cash=1_000_000)
 engine.add_data(data)
-engine.add_strategy(SMACrossoverStrategy())
-
+engine.add_strategy(HalfwayStrategy())
 stats = engine.run()
+engine.plot()
 
 # print
 print("")
@@ -27,5 +25,7 @@ print("")
 for stat, value in stats.items():
     print("{}: {}".format(stat, round(value, 5)))
 print("")
-plt.plot(data['Close'])
+
+# plt.plot(data['Close'])
 plt.show()
+
--- HalfwayStrategy.py
+++ HalfwayStrategy.py
@@ -1,12 +1,32 @@
 from BaselineStrategy import BaselineStrategy
 
-class SMACrossoverStrategy(BaselineStrategy):
+class HalfwayStrategy(BaselineStrategy):
 
     def __init__(self):
         super().__init__()
+
+        self.barIndex = None
         self.order_size = 1
 
+    def init(self):
+        self.barIndex = -1
+
     def on_bar(self):
+
+        self.barIndex += 1
+        barIndex = self.barIndex
+
+        open = self.data.Open
+        high = self.data.High
+        low = self.data.Low
+        close = self.data.Close
+
+        is_long = self.position_size > 0
+        is_short = 0 > self.position_size
+
+
+
+        ################################################################################################################
 
         if (self.position_size == 0 and
                 self.data.loc[self.current_idx].sma_12 > self.data.loc[self.current_idx].sma_24):
