df97154330ea00a0916732f49c63e6133612ccfa
Drew Sommer <drew@sommerengineering.com>
2025-05-14 16:03:46 -0700

    refactor: Encapsulate entry/exit orders into trade

--- model/Order.py
+++ model/Order.py
@@ -1,8 +1,12 @@
 class Order:
     def __init__(self, ticker, size, side, idx, price):
         self.ticker = ticker
-        self.side = side
+        self.side = side # long, short, flat
         self.size = size
         self.idx = idx
-        self.price = price,
-        self.type = type
+        self.price = price
+
+    # string representation of class, called "dunder" for double under underscores
+    def __repr__(self):
+        return f'\nticker: {self.ticker}\nside: {self.side}\nsize: {self.size}\nidx: {self.idx}\nprice: {self.price}\n'
+
--- model/Trade.py
+++ model/Trade.py
@@ -1,12 +1,12 @@
 class Trade:
-    def __init__(self, ticker, side, size, idx, type, profit):
+    def __init__(self, ticker, side, size, idx, entry_order, exit_order):
         self.ticker = ticker
-        self.side = side
+        self.side = side # long, short
         self.size = size
         self.idx = idx
-        self.type = type
-        self.profit = profit
+        self.entry_order = entry_order
+        self.exit_order = exit_order
 
     # string representation of class, called "dunder" for double under underscores
     def __repr__(self):
-        return f'\nticker: {self.ticker}\nside: {self.side}\nsize: {self.size}\nidx: {self.idx}\ntype: {self.type}\nprice: {self.profit}\n'
+        return f'\nticker: {self.ticker}\nside: {self.side}\nsize: {self.size}\nidx: {self.idx}\nentry_order: {self.entry_order}\nexit_order: {self.exit_order}\n'
