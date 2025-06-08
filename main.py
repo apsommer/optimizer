f43a64cf1ef8f01c5d9df7ad6ddf84749996830b
Drew Sommer <drew@sommerengineering.com>
2025-06-08 20:19:01 +0200

    feat: Slide train/test sets

--- main.py
+++ main.py
@@ -16,35 +16,56 @@ csv_filename = 'data/nq_1mon.csv'
 data = repo.getOhlc(csv_filename = csv_filename) # local
 # data = repo.getOhlc() # network
 
-# isolate training set
-sep = int(0.8 * len(data))
-IS = data[:sep] # in-sample
-OS = data[sep:] # out-sample
+percent = 20
+runs = 3
 
-# run exhaustive sweep
-analyzer = Analyzer(IS, 'wfa/NQ/IS80')
-analyzer.run()
-print_metrics(analyzer.metrics)
+IS_len = int(len(data) / ((percent / 100) * runs + 1))
+OS_len = int((percent / 100) * IS_len)
 
-# get result with highes profit
-max_profit = get_max_metric(analyzer, 'profit')
-id = max_profit[0].id
+IS_start, IS_end = 0, IS_len
+OS_start, OS_end = IS_len, IS_len + OS_len
+
+for run in np.arange(0, runs, 1):
+
+    # isolate training set
+    IS = data.iloc[IS_start:IS_end]
+
+    # run exhaustive sweep
+    path = 'wfa/NQ/IS' + str(percent) + str(runs) + '/' + str(run) + '/'
+    analyzer = Analyzer(IS, path)
+    analyzer.run()
+    print_metrics(analyzer.metrics)
+
+    # get result with highest profit
+    max_profit = get_max_metric(analyzer, 'profit')
+    id = max_profit[0].id
+    params = analyzer.load_result(id)['params']
+
+    # isolate testing set
+    OS = data.iloc[OS_start:OS_end]
+
+    # run strategy blind over OS
+    strategy = LiveStrategy(OS, params)
+    engine = Engine(id, strategy)
+    engine.run()
+
+    path = 'wfa/NQ/OS' + str(percent) + str(runs) + '/'
+    engine.save(path)
+
+    # print engine metrics
+    print_metrics(engine.metrics)
+
+    # slide window forward
+    IS_start += OS_len
+    IS_end += OS_len
+    OS_start += OS_len
+    OS_end += OS_len
+
+# print_metrics(engine.metrics)
+# engine.print_trades()
 # engine = analyzer.rebuild_engine(id)
-
-params = analyzer.load_result(id)['params']
-strategy = LiveStrategy(OS, params)
-engine = Engine(id, strategy)
-
-engine.run()
-engine.save('wfa/NQ/OS20')
-
-# print engine metrics
-print_metrics(engine.metrics)
-engine.print_trades()
-
-# plot strategy and equity
-plot_equity(engine)
-plot_strategy(engine)
+# plot_equity(engine)
+# plot_strategy(engine)
 
 end_time = time.time()
 print(f'\nElapsed time: {round(end_time - start_time)} seconds')
