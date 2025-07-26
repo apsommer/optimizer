14b5e2ef42c80303b204d9c548bf8326462f3d77
Drew Sommer <drew@sommerengineering.com>
2025-07-26 10:51:54 +0200

    refactor: Align walk forward architecture

--- walk_forward.py
+++ walk_forward.py
@@ -1,4 +1,5 @@
 import shutil
+import time
 import warnings
 from multiprocessing import Pool
 
@@ -13,54 +14,56 @@ from utils.utils import *
 # INPUT ###########################################################
 
 # data, indicators
-num_months = 20
+num_months = 9
 isNetwork = False
-shouldBuildEmas = False
-shouldBuildFractals = False
 
-# walk forward
+# walk forward params
 percent = 20
 runs = 14 # + 1 added later for final in-sample, use 15 of 16 cores available
 
 # analyzer
 opt = LiveParams(
     fastMinutes = [25],
-    disableEntryMinutes = [95, 105, 125],
-    fastMomentumMinutes = [65, 75, 85, 95, 105, 115, 125],
+    disableEntryMinutes = [75],
+    fastMomentumMinutes = np.around(np.linspace(55, 135, 17), 2),
     fastCrossoverPercent = [0],
-    takeProfitPercent = [.35, .45, .55],
+    takeProfitPercent = np.around(np.linspace(.25, .75, 11), 2),
+    stopLossPercent = np.around(np.linspace(.25, 1, 16), 2),
     fastAngleFactor = [0],
-    slowMinutes = [2555],
-    slowAngleFactor = [15, 20],
-    coolOffMinutes = [5],
-    trendStartHour = [0],
-    trendEndHour = [48, 60],
+    slowMinutes = [2405],
+    slowAngleFactor = [15],
+    coolOffMinutes = [25],
+    trendStartHour = [4],
+    trendEndHour = [45],
 )
 
 ###################################################################
 
 os.system('clear')
 warnings.filterwarnings('ignore')
+np.set_printoptions(threshold = 3)
 start_time = time.time()
 
 # organize outputs
 data_name = 'NQ_' + str(num_months) + 'mon'
-parent_path = 'wfa/' + data_name
+csv_filename = 'data/' + data_name + '.csv'
+parent_path = 'walk_forward/' + data_name
 path = parent_path + '/' + str(percent) + '_' + str(runs)
 
 # get ohlc prices
 data = utils.getOhlc(num_months, isNetwork)
 
-# build indicators
-if shouldBuildEmas or shouldBuildFractals:
-    print(f'Indicators:')
-if shouldBuildEmas: build_emas(data, parent_path)
-if shouldBuildFractals: build_fractals(data, parent_path)
+# get indicators
+check_indicators(data, opt, parent_path)
 emas = unpack('emas', parent_path)
 fractals = unpack('fractals', parent_path)
 
 # remove any residual analyses
 shutil.rmtree(path, ignore_errors=True)
+
+# multiprocessing uses all cores
+cores = multiprocessing.cpu_count() # 16 available
+cores -= 1 # leave 1 for basic computer tasks
 
 # init walk forward
 wfa = WalkForward(
@@ -71,14 +74,10 @@ wfa = WalkForward(
     emas = emas,
     fractals = fractals,
     opt = opt,
-    path = path)
+    path = path,
+)
 
-# multiprocessing uses all cores
-cores = multiprocessing.cpu_count() # 16 available
-cores -= 1 # leave 1 for basic computer tasks
-fitnesses = [fitness for fitness in Fit]
-
-# print header metrics
+# init header metrics
 print_metrics(wfa.metrics)
 
 # run in-sample sweep
@@ -98,6 +97,7 @@ pool.close()
 pool.join()
 
 # build composite engines
+fitnesses = [fitness for fitness in Fit]
 pool = Pool(
     processes = cores,
     initializer = set_process_name)
