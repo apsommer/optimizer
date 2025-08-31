23052c5365c03f387e756c1f83799da24618bf09
Drew Sommer <drew@sommerengineering.com>
2025-08-31 10:58:29 +0200

    feat: Complete rebuild previous wfa

--- rebuild_wfa.py
+++ rebuild_wfa.py
@@ -40,49 +40,23 @@ metrics = wfa['metrics']
 
 # display analysis metrics
 print_metrics(metrics)
-#
-# # unpack winning solution
-# winner_metric = max(best_engines, key = lambda it: it.value)
-# winner_generation = best_engines.index(winner_metric)
-# winner_id = 'g' + str(winner_generation) + 'e' + str(winner_metric.id)
-#
-# if engine is not None:
-#     winner_id = engine
-#
-# winner = unpack(winner_id, path)
-# params = winner['params']
-# cash_series = winner['cash_series']
-# trades = winner['trades']
-#
-# # build winning engine, but don't run!
-# strategy = LiveStrategy(data, emas, fractals, params)
-# engine = Engine(winner_id, strategy)
-# engine.cash_series = cash_series
-# engine.trades = trades
-# engine.analyze()
-#
-# # display winner
-# engine.print_metrics()
-# engine.print_trades()
-# engine.plot_trades()
-# ax = engine.plot_equity(shouldShow = False)
-#
-# # plot equity of best engines
-# for generation, metric in enumerate(best_engines):
-#
-#     # unpack full results
-#     id = 'g' + str(generation) + 'e' + str(metric.id)
-#     engine = unpack(id, path)
-#
-#     fplt.plot(
-#         engine['cash_series'],
-#         color = colors[generation],
-#         width = 2,
-#         ax = ax)
-#
-#     # format legend
-#     legend = '<span style="font-size:16pt">' + id + '</span>'
-#     fplt.legend_text_color = colors[generation]
-#     fplt.add_legend(legend, ax)
-#
-# fplt.show()
+
+# unpack winning solution
+winner_id = 'correlation' # todo persist in wfa.save()
+winner = unpack(winner_id, path)
+params = winner['params']
+cash_series = winner['cash_series']
+trades = winner['trades']
+
+# build winning engine, but don't run!
+strategy = LiveStrategy(data, emas, fractals, params)
+engine = Engine(winner_id, strategy)
+engine.cash_series = cash_series
+engine.trades = trades
+engine.analyze()
+
+# display winner
+engine.print_metrics()
+engine.print_trades()
+engine.plot_trades()
+ax = engine.plot_equity()
